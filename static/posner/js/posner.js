263acc4abfacdc5ec4cc59bf542302be259254d2
Michael Loftus <loftusmi@umich.edu>
2025-05-20 12:33:46 -0400

    Refactored; no square

--- static/posner/js/posner.js
+++ static/posner/js/posner.js
@@ -1,9 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     
   const TOTAL_TRIALS = 10;
-  const TOTAL_SESSIONS = 2;
-
-  let session_count = 0;
 
   // 3FFFF - two common
   // 6FFFF - left uncommon
@@ -11,11 +8,14 @@ document.addEventListener('DOMContentLoaded', () => {
   // FFFFF - two uncommon
   const color_codes = ['#333333', '#555555', '#888888', '#CCCCCC'];
   
+
+  // Load page elements
   const instructionsScreen = document.getElementById('instructions-screen');
   const startButton1 = document.getElementById('start-button1');
   const startButton2 = document.getElementById('start-button2');
   const experimentArea = document.getElementById('experiment-area');
   const pauseScreen = document.getElementById('pause-screen');
+  pauseScreen.classList.add('hidden');
   const endScreen = document.getElementById('end-screen');
   const trialCounterElement = document.getElementById('trial-counter');
   const totalTrialsDisplayElement = document.getElementById('total-trials-display');
@@ -23,6 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
   
   const cueDisplayElement = document.getElementById('cue-display');
   const cueShapeElement = document.getElementById('cue-shape');
+  cueShapeElement.className = 'cue-element fixation-cross'; // Set to be a cross
 
   const leftDotElement = document.getElementById('left-dot');
   cueDisplayElement.appendChild(leftDotElement);
@@ -30,91 +31,71 @@ document.addEventListener('DOMContentLoaded', () => {
   const rightDotElement = document.getElementById('right-dot');
   cueDisplayElement.appendChild(rightDotElement);
 
-  const cornerSquareElement = document.createElement('div');
-  document.body.appendChild(cornerSquareElement);
-  console.log("Corner square initialized.");
+  const cornerSquareElement = document.getElementById('corner-square');
 
-  startButton1.addEventListener('click', () => {
+  console.log("Page elements initialized.");
+
+
+  // Start button event listeners
+  startButton1.addEventListener('click', async () => {
     instructionsScreen.classList.add('hidden');
-    pauseScreen.classList.add('hidden');
-    // For now, we directly run the fixation cross sequence.
-    runFixationCrossSequence();
-    session_count++;
-  });
-  startButton2.addEventListener('click', () => {
-    instructionsScreen.classList.add('hidden');
-    pauseScreen.classList.add('hidden');
-    // For now, we directly run the fixation cross sequence.
-    runFixationCrossSequence();
-    session_count++;
+    await startSession();
   });
 
-  async function runFixationCrossSequence(trials = 10, sessions = 2) {
+  startButton2.addEventListener('click', async () => {
+    instructionsScreen.classList.add('hidden');
+    pauseScreen.classList.add('hidden');
+    await startSession();
+    endScreen.classList.remove('hidden');
+    cornerSquareElement.classList.add('hidden');
+  });
+
+
+  async function startSession(trials = TOTAL_TRIALS) {
       
-    cornerSquareElement.style.visibility = 'visible';
+    cornerSquareElement.classList.remove('hidden');
   
     // Make sure experiment area is visible and cue display is ready
     if (experimentArea && cueDisplayElement && cueShapeElement) {
         experimentArea.classList.remove('hidden');
         cueDisplayElement.classList.add('hidden'); // Ensure it's hidden initially
-            
-        // Set up the fixation cross
-        cueShapeElement.className = 'cue-element fixation-cross'; // Set to be a cross
         
+        const trialOrder = generateTrialOrder();
+
+        experimentUUID = generateUUID(); // From utils.js
+        const sessionData = {
+          trials: trialOrder,
+          experiment_name: 'posner',
+          experiment_date: new Date().toISOString(),
+          total_trials: trials,
+          percent_uncommon: 0.15,
+          experiment_uuid: experimentUUID,
+          experiment_url: window.location.href,
+          experiment_user_agent: navigator.userAgent,
+          experiment_screen_resolution: `${window.screen.width}x${window.screen.height}`,
+        }
+
         // Run trials
-        runTrials(trials, sessions);
-    
+        await runTrials(trialOrder);
+
+        pauseScreen.classList.remove('hidden');
+        cornerSquareElement.classList.add('hidden');
+
+        // Send session data to server
+        await sendDataToServer(sessionData, 'posner');
+        
     } else {
         console.error("Required elements for fixation cross not found.");
     }
   }
-  
-  // Randomize color appearance on left and right sides
-  function generateTrialOrder() {
-    let trials = [];
-    let finalTrials = [];
-  
-    // 15% uncommon trials
-    for (let i = 0; i < 0.15*TOTAL_TRIALS; i++) {
-        trials.push(1);
-    }
-    // 85% common trials
-    for (let i = 0; i < 0.85*TOTAL_TRIALS; i++) {
-        trials.push(0);
-    }
-    
-    shuffle(trials);
-    // Push pushes a refernce, not a deep copy
-    finalTrials.push(trials);
-  
-    trials_copy = JSON.parse(JSON.stringify(trials));
-    shuffle(trials_copy);
-    finalTrials.push(trials_copy);
-  
-    return finalTrials;
-  }
-  
-  async function runTrials(trials, sessions) {
+
+
+  async function runTrials(trialOrder) {
   
     let count = 0;
     let color_code = 0;
   
-    // Initialize trials
-    const trialOrder = generateTrialOrder();
-  
-    experimentUUID = generateUUID(); // From utils.js
-  
-    const sessionData = {
-        trials: trialOrder,
-        experiment_name: 'posner',
-        experiment_date: new Date().toISOString(),
-        total_trials: trials,
-        percent_uncommon: 0.15,
-        experiment_uuid: experimentUUID,
-        experiment_url: window.location.href,
-        experiment_user_agent: navigator.userAgent,
-        experiment_screen_resolution: `${window.screen.width}x${window.screen.height}`,
-    }
+    let trials = trialOrder[0].length;
   
     for (; count < trials; count++) {
       color_code = 0;
@@ -141,7 +122,7 @@ document.addEventListener('DOMContentLoaded', () => {
       }
   
       cueDisplayElement.classList.remove('hidden');        // show fixation cross
-      cornerSquareElement.style.visibility = 'hidden';      // hide corner square
+      cornerSquareElement.classList.add('hidden');      // hide corner square
   
       // Wait 500ms (fixation)
       await new Promise(r => setTimeout(r, 500));
@@ -153,7 +134,7 @@ document.addEventListener('DOMContentLoaded', () => {
       // Show dots and corner square
       leftDotElement.style.display = 'block';
       rightDotElement.style.display = 'block';
-      cornerSquareElement.style.visibility = 'visible';
+      cornerSquareElement.classList.remove('hidden');
       cornerSquareElement.style.backgroundColor = color_codes[color_code];
   
       // Dots visible for 250ms
@@ -172,15 +153,31 @@ document.addEventListener('DOMContentLoaded', () => {
   
     // After all trials
     experimentArea.classList.add('hidden');
+  }
+
+
+  // Randomize color appearance on left and right sides
+  function generateTrialOrder() {
+    let trials = [];
+    let finalTrials = [];
   
-    if (session_count === sessions) {
-      endScreen.classList.remove('hidden');
-      cornerSquareElement.style.visibility = 'hidden';
-    } else {
-      pauseScreen.classList.remove('hidden');
-      cornerSquareElement.style.visibility = 'hidden';
+    // 15% uncommon trials
+    for (let i = 0; i < 0.15*TOTAL_TRIALS; i++) {
+        trials.push(1);
     }
+    // 85% common trials
+    for (let i = Math.ceil(0.15*TOTAL_TRIALS); i < TOTAL_TRIALS; i++) {  
+      trials.push(0);
+    }
+    
+    shuffle(trials);
+    // Push pushes a refernce, not a deep copy
+    finalTrials.push(trials);
   
-    sendDataToServer(sessionData, 'posner'); // send JSON to server
+    trials_copy = JSON.parse(JSON.stringify(trials));
+    shuffle(trials_copy);
+    finalTrials.push(trials_copy);
+  
+    return finalTrials;
   }
 });
