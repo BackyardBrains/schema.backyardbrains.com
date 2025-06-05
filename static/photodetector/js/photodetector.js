428aed54b682c800b59fec9557711933889b486e
Michael Loftus <loftusmi@umich.edu>
2025-06-05 15:13:59 -0400

    Randomized colors, instant fall and rise, changing duration

--- static/photodetector/js/photodetector.js
+++ static/photodetector/js/photodetector.js
@@ -5,42 +5,60 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   console.log("Page elements initialized.");
 
-  const delays = [250, 500, 1000]
+  const delays = [500, 250, 100, 50, 0]
 
   session = 0;
 
   // Start button event listeners
   startButton1.addEventListener('click', async () => {
-    if(session < 3)
+    if(session < delays.length)
     {
       await startSession(body, delays[session]);
     ++session;
-    console.log(`Session ${session} 1 Complete`);
+    console.log(`Session ${session} Complete`);
     }
   })
 });
 
 async function startSession(element, delay) {
 
-  for(let trial = 0; trial < 100; ++trial)
-    for(let i = 0; i < 5; ++i) {
-      for(let j = i; j < 5; ++j) {
+  let trial_n = 100;
+  let color_idx = [1,2,3,4,5];
+
+  console.log(`Delay: ${delay} ms`)
+
+  for(let trial = 0; trial < 100; ++trial){
+    
+    color_idx = shuffleArray(color_idx);
+
+    for(let i = 0; i < 4; ++i) {
+      for(let j = i+1; j < 5; ++j) {
 
         // Starting color
-        element.style.backgroundColor = find_color(i);
+        element.style.backgroundColor = find_color(color_idx[i]);
         // Wait to transition
         await new Promise(r => setTimeout(r, delay));
         // Transition to next color
-        element.style.backgroundColor = find_color(j);
+        element.style.backgroundColor = find_color(color_idx[j]);
         // Wait for detection
         await new Promise(r => setTimeout(r, delay));
         // Return to original color at beginning of next loop
       }
     }
+    console.log(`Completed trial ${trial} out of ${trial_n}.`)
+  }
 }
 
 function find_color(color_code) {
   const COLOR_CODES = ['#FFFFFF','#EEEEEE', '#CBCBCB', '#9F9F9F', '#7D7D7D'];
 
   return COLOR_CODES[color_code];
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
