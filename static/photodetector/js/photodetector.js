9a32a465d69eb4be40d51fa7026befddb1a5a4e0
Michael Loftus <loftusmi@umich.edu>
2025-06-06 16:49:04 -0400

    Always transitioning

--- static/photodetector/js/photodetector.js
+++ static/photodetector/js/photodetector.js
@@ -5,31 +5,36 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   console.log("Page elements initialized.");
 
-  const delays = [250, 100, 50]
+  const hightimes = [250, 100, 50]
 
   session = 0;
 
   // Start button event listeners
   startButton1.addEventListener('click', async () => {
-    if(session < delays.length)
+    if(session < hightimes.length)
     {
-      await startSession(body, delays[session]);
+      await startSession(body, hightimes[session]);
     ++session;
     console.log(`Session ${session} Complete`);
     }
   })
 });
 
-async function startSession(element, delay) {
+async function startSession(element, hightime) {
 
   let trial_n = 100;
   eventOrder = []
   
-  console.log(`Delay: ${delay} ms`)
+  console.log(`Hightime: ${hightime} ms`)
+
+  start_color = '#FFFFFF'
 
   for(let trial = 0; trial < trial_n; ++trial) {
 
-    let color_idx = shuffleArray([0,1,2,3,4]);
+    let color_idx = shuffleArray([0,1,2,3,4])
+    while (start_color === find_color(color_idx[0])) {
+      color_idx = shuffleArray([0,1,2,3,4])
+    }
 
     for(let i = 0; i < 4; ++i) {
       for(let j = i+1; j < 5; ++j) {
@@ -39,23 +44,24 @@ async function startSession(element, delay) {
         console.log(color_idx[i]+1);
         eventOrder.push(color_idx[i]+1);
         // Wait to transition
-        await new Promise(r => setTimeout(r, delay));
+        await new Promise(r => setTimeout(r, hightime));
         // Transition to next color
         element.style.backgroundColor = find_color(color_idx[j]);
         console.log(color_idx[j]+1);
         eventOrder.push(color_idx[j]+1);
         // Wait for detection
-        await new Promise(r => setTimeout(r, delay));
+        await new Promise(r => setTimeout(r, hightime));
         // Return to original color at beginning of next loop
       }
     }
 
     console.log(`Completed trial ${trial+1} out of ${trial_n}.`)
+    start_color = find_color(color_idx[4])
   }
 
   const experimentUUID = generateUUID();
   const dataToSend = {
-    hightime: delay,
+    hightime: hightime,
     eventOrder: eventOrder
   }
   sendDataToServer(dataToSend, experimentUUID, "photodetector");
